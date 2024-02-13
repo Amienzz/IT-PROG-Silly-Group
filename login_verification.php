e6b7446b1d5095647b05e953bede4ede7943ab45
qmarkm <106965891+qmarkm@users.noreply.github.com>
2024-02-13 22:04:23 +0800

    Designed index.html and create_account.html files

--- login_verification.php
+++ login_verification.php
@@ -33,6 +33,7 @@ if ($status == 1) {
     <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Login Verification</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form action="mainpage.php" method="post">
