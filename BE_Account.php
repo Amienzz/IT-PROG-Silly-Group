db4b17db005794e1a9509e91374c2e2b00feea21
KallavanIVCT <132966015+KallavanIVCT@users.noreply.github.com>
2024-03-29 23:12:50 +0800

    Added function "get_account_list"

--- BE_Account.php
+++ BE_Account.php
@@ -1,8 +1,7 @@
 <?php
-include_once 'Back_database.php';
+include 'Back_database.php';
 ?>
-
-
+ 
 <?php
      class create_acc extends DatabaseConn
     {
@@ -11,16 +10,16 @@ include_once 'Back_database.php';
             $smt = $this->conn->prepare("SELECT username FROM users WHERE username = ? ");
             $smt->bind_param("s",$s_username);
             $smt->execute();
-
+ 
             $result = $smt->get_result();
-
-            if($result === null) 
+ 
+            if($result === null) ///returns 1 if false
             {
                 return 1;
             }
             else
             {
-                return 0;
+                return 0;   /// returns 0 if true
             }
         }
         public function isEmail_taken($s_email)////checks if the email is taken
@@ -28,9 +27,9 @@ include_once 'Back_database.php';
             $smt = $this->conn->prepare("SELECT email FROM users WHERE email = ? ");
             $smt->bind_param("s",$s_email);
             $smt->execute();
-
+ 
             $result = $smt->get_result();
-
+ 
             if($result === null) 
             {
                 return 1;
@@ -53,13 +52,13 @@ include_once 'Back_database.php';
         }
         public function log_in($s_email, $s_password)
         {
-            $smt = $this->conn->prepare("SELECT email FROM users WHERE email = ? and password = ? ");
+            $smt = $this->conn->prepare("SELECT email FROM user WHERE email = ? AND password = ? ");
             $smt->bind_param("ss",$s_email, $s_password);
             $smt->execute();
-
+ 
             $result = $smt->get_result();
             $data = $result->fetch_assoc();
-
+ 
             if($data)
             {
                 return 1; ///returns 1 if password and email are the same;
@@ -74,18 +73,97 @@ include_once 'Back_database.php';
             $date = date('Y-m-d'); // Format: YYYY-MM-DD
             return $date;
         }
-        public function register_user($s_firstn, $s_last, $mid, $gender, $birthdate, $email, $s_password)
+        public function register_user($s_firstn, $s_last, $mid, $gender, $birthdate, $s_userid, $s_email, $s_username, $s_user_password, $s_bio, $account_type)
         {
             $date = date_today();
-
-            $smt = $this->conn->prepare("INSERT INTO user (first_name, last_name, middle_initial, gender, birthday, email, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-            $smt->bind_param("ssssssss", $s_firstn, $s_last, $mid, $gender, $birthdate, $email, $s_password, $date);
-            
-            $smt->execute();
  
-            $smt->close();
-
+            $smt = $this->conn->prepare("INSERT INTO user (first_name, last_name, middle_initial, gender, birthday,user_id, email, username,user_password,registration_date,bio,account_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+            $smt->bind_param("ssssssssssss", $s_firstn, $s_last, $mid, $gender, $birthdate,$s_userid,$s_email, $s_username, $s_user_password, $date, $s_bio, $account_type);
+            try
+            {
+                $smt->execute();
+                $smt->close(); 
+            }
+            catch(Exception $e)
+            {
+                return 0;
+            }
+ 
         }
-    }
+        public function isBusines($s_username)
+        {
+            $smt  = $this->conn->prepare("SELECT account_type FROM users WHERE username = ?");
+            $smt -> bind_param("s", $s_username);
+            $smt -> execute();
+ 
+            $result = $smt->get_result();
+            if(!(strcmp($result,"business")))   //return 1 if business
+            {
+                return 1;
+            }
+            else    //return 0 if not
+            {
+                return 0;
+            }
+        }
+        public function searchEmail($s_email) //getUser via email
+        {
+            try
+            {
+                $stmt = $this->conn->prepare("SELECT first_name, last_name, middle_initial, gender, birthday, user_id, email, username, registration_date, description, account_type FROM user WHERE email = ?");
+                $stmt->bind_param("s",$s_email);
+                $stmt->execute();
+ 
+                $result = $stmt->get_result();
+                $data = array();
+ 
+                while( $row = $result->fetch_assoc())
+                {
+                    $data = $row;
+                }
+                return $data;
+            }
+            catch(Exception $e)
+            {
+                return 0;
+            }
+        }
+        public function searchID($s_id) //getUser via ID
+        {
+            try
+            {
+                $stmt = $this->conn->prepare("SELECT first_name, last_name, middle_initial,gender,birthday,user_id,email,username,registration_date,description,account_type FROM user WHERE user_id = ?");
+                $stmt->bind_param("s",$s_id);
+                $stmt->execute();
+ 
+                $result = $stmt->get_result();
+                $data = array();
+ 
+                while( $row = $result->fetch_assoc())
+                {
+                    $data = $row;
+                }
+                return $data;
+            }
+            catch(Exception $e)
+            {
+                return 0;
+            }
+        }
 
+        public function get_account_list()
+        {
+            $stmt = $this->conn->prepare("SELECT * FROM user");
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $all_data = array();
+            while ($row = $result->fetch_assoc())
+            {
+                $all_data[] = $row;
+            }
+    
+            return $all_data;
+        }
+
+    }       
 ?>
